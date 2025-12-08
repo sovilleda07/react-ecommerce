8344d15ac221674cf9fb56b3aba30897e3fe5852
sovilleda07 <sovilleda07@gmail.com>
2025-12-07 20:58:09 -0600

    Add icons

--- src/pages/TrackingPage.jsx
+++ src/pages/TrackingPage.jsx
@@ -4,6 +4,7 @@ import './TrackingPage.css';
 export function TrackingPage() {
   return (
     <>
+      <link rel="icon" type="image/svg+xml" href="/tracking-favicon.png" />
       <title>Tracking</title>
 
       <Header />
--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -5,6 +5,7 @@ import './CheckoutHeader.css';
 export function CheckoutPage() {
   return (
     <>
+      <link rel="icon" type="image/svg+xml" href="/cart-favicon.png" />
       <title>Checkout</title>
 
       <CheckoutHeader />
