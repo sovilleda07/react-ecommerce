00e750ab9009f107b037d7dc56d0c2194298741a
sovilleda07 <sovilleda07@gmail.com>
2025-12-08 20:55:02 -0600

    Fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,12 @@ import { products } from '../data/products';
 import './HomePage.css';
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products').then((response) => {
+    return response.json();
+  }).then((data) => {
+    console.log(data);
+  })
+
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
