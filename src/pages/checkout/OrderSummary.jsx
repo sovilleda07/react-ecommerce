01cf5837207f3bfff8808fb975ff3478b39425b9
sovilleda07 <sovilleda07@gmail.com>
2025-12-10 21:54:07 -0600

    Separate into component order delivery date

--- src/pages/checkout/OrderSummary.jsx
+++ src/pages/checkout/OrderSummary.jsx
@@ -1,20 +1,14 @@
-import dayjs from "dayjs";
 import { DeliverOptions } from "./DeliveryOptions";
+import { DeliveryDate } from "./DeliveryDate";
 import { CartItemDetails } from "./CartItemDetails"
 
 export function OrderSummary({ cart, deliveryOptions }) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 && cart.map((cartItem) => {
-        const selectedDeliveryOption = deliveryOptions.find((deliveryOption) => {
-          return deliveryOption.id == cartItem.deliveryOptionId
-        });
-
         return (
           <div key={cartItem.id} className="cart-item-container">
-            <div className="delivery-date">
-              Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-            </div>
+            <DeliveryDate cartItem={cartItem} deliveryOptions={deliveryOptions} />
 
             <div className="cart-item-details-grid">
               <CartItemDetails cartItem={cartItem} />
