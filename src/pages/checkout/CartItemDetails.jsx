e8c21af94924fff8bef95a384ccc11d3a62c434e
sovilleda07 <sovilleda07@gmail.com>
2025-12-12 21:26:31 -0600

    Make update quantity interactive in checkout

--- src/pages/checkout/CartItemDetails.jsx
+++ src/pages/checkout/CartItemDetails.jsx
@@ -1,7 +1,29 @@
 import axios from "axios";
+import { useState } from "react";
 import { formatMoney } from "../../utils/money"
 
 export function CartItemDetails({ cartItem, loadCart }) {
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+  const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
+
+  const updateQuantity = async () => {
+    setIsUpdatingQuantity(!isUpdatingQuantity);
+
+    if (isUpdatingQuantity) {
+      await axios.put(`/api/cart-items/${cartItem.productId}`, {
+        quantity
+      });
+
+      await loadCart();
+
+      setIsUpdatingQuantity(false)
+    }
+  };
+
+  const updateQuantityInput = (event) => {
+    setQuantity(Number(event.target.value));
+  }
+
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
 
@@ -22,9 +44,12 @@ export function CartItemDetails({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity: {isUpdatingQuantity
+              ? <input type="text" className="quantity-input" value={quantity} onChange={updateQuantityInput} />
+              : <span className="quantity-label">{cartItem.quantity}</span>
+            }
           </span>
-          <span className="update-quantity-link link-primary">
+          <span className="update-quantity-link link-primary" onClick={updateQuantity}>
             Update
           </span>
           <span className="delete-quantity-link link-primary" onClick={deleteCartItem}>
