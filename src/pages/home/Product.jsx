893f5a75960b5129d14d418d682fb9514c68254e
sovilleda07 <sovilleda07@gmail.com>
2025-12-12 21:02:43 -0600

    Show added message

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -5,13 +5,21 @@ import CheckmarkIcon from '../../assets/images/icons/checkmark.png';
 
 export function Product({ product, loadCart }) {
   const [quantity, setQuantity] = useState(1);
+  const [showAddedMessage, setShowAddedMessage] = useState(false);
 
   const addToCart = async () => {
     await axios.post('/api/cart-items', {
       productId: product.id,
       quantity
     });
+
     await loadCart();
+
+    setShowAddedMessage(true);
+
+    setTimeout(() => {
+      setShowAddedMessage(false);
+    }, 2000);
   };
 
   const selectQuantity = (event) => {
@@ -59,7 +67,7 @@ export function Product({ product, loadCart }) {
 
       <div className="product-spacer"></div>
 
-      <div className="added-to-cart">
+      <div className="added-to-cart" style={{ opacity: showAddedMessage ? 1 : 0 }}>
         <img src={CheckmarkIcon} />
         Added
       </div>
