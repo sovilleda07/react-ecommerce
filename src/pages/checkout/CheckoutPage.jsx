4232cae51e6e20c3254589dc834f67ee656f4024
sovilleda07 <sovilleda07@gmail.com>
2025-12-12 20:54:33 -0600

    Separate fetch checkout data

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -13,14 +13,20 @@ export function CheckoutPage({ cart, loadCart }) {
 
   useEffect(() => {
     const fetchCheckoutData = async () => {
-      let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
+      const response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
       setDeliveryOptions(response.data);
-
-      response = await axios.get('/api/payment-summary')
-      setPaymentSummary(response.data);
     };
 
     fetchCheckoutData();
+  }, []);
+
+  useEffect(() => {
+    const fetchPaymentSummary = async () => {
+      const response = await axios.get('/api/payment-summary')
+      setPaymentSummary(response.data);
+    }
+
+    fetchPaymentSummary();
   }, [cart]);
 
   return (
