2d9c273069c7dcaad293504f04257fba219bc227
sovilleda07 <sovilleda07@gmail.com>
2025-12-15 22:21:56 -0600

    Add test to DeliveryOptions component and fix the component name

--- src/pages/checkout/OrderSummary.jsx
+++ src/pages/checkout/OrderSummary.jsx
@@ -1,4 +1,4 @@
-import { DeliverOptions } from "./DeliveryOptions";
+import { DeliveryOptions } from "./DeliveryOptions";
 import { DeliveryDate } from "./DeliveryDate";
 import { CartItemDetails } from "./CartItemDetails"
 
@@ -13,7 +13,7 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
             <div className="cart-item-details-grid">
               <CartItemDetails cartItem={cartItem} loadCart={loadCart} />
 
-              <DeliverOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart} />
+              <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart} />
             </div>
           </div>
         )
