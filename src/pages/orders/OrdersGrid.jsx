234b415ebaec9d956f2f758d3a172fde78f9c1a7
sovilleda07 <sovilleda07@gmail.com>
2025-12-15 22:39:03 -0600

    Add tests to OrderPage component

--- src/pages/orders/OrdersGrid.jsx
+++ src/pages/orders/OrdersGrid.jsx
@@ -6,7 +6,7 @@ export function OrdersGrid({ orders, loadCart }) {
     <div className="orders-grid">
       {orders.map((order) => {
         return (
-          <div key={order.id} className="order-container">
+          <div key={order.id} className="order-container" data-testid="order-container">
             <OrdersHeader order={order} />
 
             <OrderDetailsGrid order={order} loadCart={loadCart} />
