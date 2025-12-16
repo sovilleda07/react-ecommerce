de911134363fa51ed97f3f7e7f3bc677579b1d35
sovilleda07 <sovilleda07@gmail.com>
2025-12-15 22:29:14 -0600

    Add tests to OrderSummary component

--- src/pages/checkout/CartItemDetails.jsx
+++ src/pages/checkout/CartItemDetails.jsx
@@ -42,18 +42,17 @@ export function CartItemDetails({ cartItem, loadCart }) {
 
   return (
     <>
-      <img className="product-image"
-        src={cartItem.product.image} />
+      <img className="product-image" data-testid="cart-item-image" src={cartItem.product.image} />
 
       <div className="cart-item-details">
-        <div className="product-name">
+        <div className="product-name" data-testid="cart-item-name">
           {cartItem.product.name}
         </div>
-        <div className="product-price">
+        <div className="product-price" data-testid="cart-item-price">
           {formatMoney(cartItem.product.priceCents)}
         </div>
         <div className="product-quantity">
-          <span>
+          <span data-testid="cart-item-quantity">
             Quantity: {isUpdatingQuantity
               ? <input type="text" className="quantity-input" value={quantity} onChange={updateQuantityInput} onKeyDown={handleQuantityKeyDown} />
               : <span className="quantity-label">{cartItem.quantity}</span>
@@ -62,7 +61,7 @@ export function CartItemDetails({ cartItem, loadCart }) {
           <span className="update-quantity-link link-primary" onClick={updateQuantity}>
             Update
           </span>
-          <span className="delete-quantity-link link-primary" onClick={deleteCartItem}>
+          <span className="delete-quantity-link link-primary" data-testid="cart-item-delete-quantity-link" onClick={deleteCartItem}>
             Delete
           </span>
         </div>
