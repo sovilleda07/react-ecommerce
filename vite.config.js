98ab5e4be474c86bc11c16a0f71aa94840bc8214
sovilleda07 <sovilleda07@gmail.com>
2026-01-30 22:12:28 -0600

    Remove the axios proxy from Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000'
-      },
-      '/images': {
-        target: 'http://localhost:3000'
-      }
-    }
-  }
+  plugins: [react()]
 })
